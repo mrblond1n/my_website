e821eaa67a8693a83c48ba7b13a412817b12d5f9
Nikita Kirdiapin <nikita.kirdiapin@gmail.com>
2020-04-02 16:35:48 +0300

    update

--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,5 @@
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
-import VuetifyLogo from "~/components/VuetifyLogo.vue";
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
-};
+export default {};
 </script>

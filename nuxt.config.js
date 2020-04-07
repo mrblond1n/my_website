4c9608aebc28976e375a8b4e92101eff9c053cb7
Nikita Kirdiapin <nikita.kirdiapin@gmail.com>
2020-04-07 09:30:25 +0300

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,8 +12,8 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate: "%s - " + process.env.npm_package_name,
-    title: process.env.npm_package_name || "",
+    titleTemplate: "%s - " + 'website',
+    title: 'Nikita Kirdiapin',
     meta: [
       { charset: "utf-8" },
       {
